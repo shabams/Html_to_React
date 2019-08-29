38c15d01b80cc2a77e82fd6d7fc8b3adbf791204
fancykingstar <fancykingstar2019@gmail.com>
2019-08-29 06:49:55 -0400

    complte booking2

--- src/Client/Components/App/Booking/Booking1.js
+++ src/Client/Components/App/Booking/Booking1.js
@@ -1,10 +1,49 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
+import { Row, Col, Image, Form, Button } from 'react-bootstrap';
+import booking1_image from '../../../Assets/Images/booking1-image.png';
 
 class Booking1 extends React.Component {
 	render() {
 		return(
-			<></>
+			<>
+				<div className="booking1" style={{ maxWidth: 1280 }}>
+					<div className='booking1-body d-flex'>
+						<div className='booking1-image d-flex' style={{ padding: 0 }}>
+							<div>
+								<Image src={booking1_image} style={{ maxWidth: 522, maxHeight: 503, width: '100%' }} />
+							</div>
+						</div>
+
+						<div className='booking1-context d-flex justify-content-center' style={{ flexDirection: 'column' }}>
+							<h1 className='booking1-context-title' style={{ color: '#12261f' }}>Find Your Booking</h1>
+							<h4 style={{ color: '#12261f' }}>Use your email and phone number to find your bookings.</h4>
+
+							<Row className='booking1-row'>
+								<Col md={4} sm={4} xm={12}>
+									<Form.Group controlId='formBasicEmailAddress'>
+										<Form.Label>Email Address*</Form.Label>
+										<Form.Control type='email' />
+									</Form.Group>
+								</Col>
+
+								<Col md={4} sm={4} xm={12}>
+									<Form.Group controlId='formBasicPhoneNumber'>
+										<Form.Label>Phone Number*</Form.Label>
+										<Form.Control type='text' />
+									</Form.Group>
+								</Col>
+
+								<Col md={4} sm={4} xm={12} style={{ alignSelf: 'flex-end', paddingBottom: '15px' }}>
+									<Button variant="primary" className='find'>
+								        Find
+								    </Button>
+								</Col>
+							</Row>
+						</div>
+					</div>
+				</div>
+			</>
 		)
 	}
 }
--- src/Client/Components/App/Booking/Booking2.js
+++ src/Client/Components/App/Booking/Booking2.js
@@ -1,60 +1,71 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
+import { Row, Form, Col, Button } from 'react-bootstrap';
 import './Booking.css';
 
 const BookingConfirming = props => {
 	return (
-		<div className='booking-confirming d-flex'>
-			<div className='booking-confirming-date d-flex'>
-				<p>THU</p>
-				<p>July 29</p>
-				<p>9 AM</p>
-			</div>
-			<div className='booking-confirming-data d-flex'>
-				<div className='name-state-numberofrooms d-flex'>
-					<div className='full-name'>	
-						<h6>Full Name</h6>
-						<h5>Fulan Al Fulani</h5>
-					</div>
-					<div className='state'>
-						<h6>State</h6>
-						<h5>Ohio</h5>
-					</div>
-					<div className='rooms'>
-						<h6>Number of rooms</h6>
-						<h5>3</h5>
-					</div>
+		<>
+			<h4 className='booking2-title'>We found 2 bookings matching your email and phone number</h4>
+			<div className='booking-confirming d-flex'>
+				<div className='booking-confirming-date d-flex'>
+					<p>THU</p>
+					<p>July 29</p>
+					<p>9 AM</p>
 				</div>
+				<div className=''>
+					<div className='booking-confirming-data d-flex'>
+						<div className='name-state-numberofrooms d-flex'>
+							<div className='full-name'>	
+								<h6>Full Name</h6>
+								<h5>Fulan Al Fulani</h5>
+							</div>
+							<div className='state'>
+								<h6>State</h6>
+								<h5>Ohio</h5>
+							</div>
+							<div className='rooms'>
+								<h6>Number of rooms</h6>
+								<h5>3</h5>
+							</div>
+						</div>
 
-				<div className='state-city-address d-flex'>
-					<div className='phone-number'>
-						<h6>Phone Number</h6>
-						<h5>+1 123 456 789</h5>
-					</div>
-					<div className='city'>
-						<h6>City</h6>
-						<h5>Oxford</h5>
-					</div>
-					<div className='Number of bathrooms'>
-						<h6>Number of bathrooms</h6>
-						<h5>1</h5>
-					</div>							
-				</div>
+						<div className='state-city-address d-flex'>
+							<div className='phone-number'>
+								<h6>Phone Number</h6>
+								<h5>+1 123 456 789</h5>
+							</div>
+							<div className='city'>
+								<h6>City</h6>
+								<h5>Oxford</h5>
+							</div>
+							<div className='Number of bathrooms'>
+								<h6>Number of bathrooms</h6>
+								<h5>1</h5>
+							</div>							
+						</div>
 
-				<div className='rooms-bathrooms d-flex'>
-					<div className='address'>
-						<h6>123, St name, lorem ipsum.</h6>
-						<h5>Oxford</h5>
+						<div className='rooms-bathrooms d-flex'>
+							<div className='address'>
+								<h6>123, St name, lorem ipsum.</h6>
+								<h5>Oxford</h5>
+							</div>
+						</div>
+					</div>
+					<div className='request-edit d-flex'>
+						<h6>Looking to edit your booking details?</h6>
+						<a href='#'>Request Edit</a>
 					</div>
 				</div>
 			</div>
-		</div>
+		</>
 	)
 }
 
 const BookingConfirmingMobile = props => {
 	return (
 		<>
+			<h4 className='booking2-title'>We found 2 bookings matching your email and phone number</h4>
 			<div className='confirmation d-flex'>
 				<div className='confirming-date d-flex'>
 					<p>THU</p>
@@ -98,6 +109,10 @@ const BookingConfirmingMobile = props => {
 							<h5>1</h5>
 						</div>
 					</div>
+					<div className='request-edit' style={{marginTop: 30}}>
+						<h6>Looking to edit your booking details?</h6>
+						<a href='#'>Request Edit</a>
+					</div>
 				</div>
 			</div>
 		</>
@@ -130,10 +145,31 @@ class Booking2 extends React.Component {
 	render() {
 		const width = this.state.width;
 		return (
-			<>
+			<div style={{ maxWidth: '860px', padding: '0 15px' }} className="booking2">
+				<Row style={{ marginBottom: 50, marginTop: 120 }} className='booking2-row'>
+					<Col>
+						<Form.Group controlId='formBasicEmailAddress'>
+							<Form.Label>Email Address*</Form.Label>
+							<Form.Control type='email' />
+						</Form.Group>
+					</Col>
+
+					<Col>
+						<Form.Group controlId='formBasicPhoneNumber'>
+							<Form.Label>Phone Number*</Form.Label>
+							<Form.Control type='text' />
+						</Form.Group>
+					</Col>
+					<Col style={{ alignSelf: 'flex-end', paddingBottom: '15px' }}>
+						<Button variant="primary" className='find'>
+					        Find
+					    </Button>
+					</Col>
+				</Row>
+				{ width <=	 650 ?  <a href='/Booking1' style={{ color: '#393c40', fontSize: '18px' }}>Back</a>  : <div /> }
 				{ width > 650 ?  <BookingConfirming /> : <BookingConfirmingMobile /> }
 				{ width > 650 ?  <BookingConfirming /> : <BookingConfirmingMobile /> }
-			</>
+			</div>
 		)
 	}
 }
